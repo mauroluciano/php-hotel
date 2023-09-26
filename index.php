7f7705ee622733c70b1da6e8ba4a99fad608a7ca
mauroluciano <mauroluciano911@gmail.com>
2023-09-26 17:29:21 +0200

    add table

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     $hotels = [
 
         [
@@ -42,27 +41,54 @@
 
 ?>
 
-<?php
-$hotels = $_GET["name"];
-$hotels = $_GET["description"];
-$hotels = $_GET["parking"];
-$hotels = $_GET["vote"];
-$hotels = $_GET["distance_to_center"];
-
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP hotel</title>
 </head>
 <body>
-    <section>
-        <?php var_dump($name); ?>
-    
-</section>
+
+
+    <div class="container">
+<table class="table">
+  <thead>
+    <tr>      
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php 
+  foreach ($hotels as $hotel){
+    echo "<tr>";
+
+    echo "<td>" . $hotel["name"] . "</td>";
+  
+    echo "<td>" . $hotel["description"] . "</td>";
+
+    echo "<td>" . $hotel["parking"] . "</td>";
+  
+    echo "<td>" . $hotel["vote"] . "</td>";
+  
+    echo "<td>" . $hotel["distance_to_center"] . "</td>";
+  
+    echo "</tr>";
+
+
+   }
+  ?>
+  </tbody>
+</table>
+</div>
+
 </body>
 </html>
